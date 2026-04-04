d59afbfcbb10677f75e6a53db183d840e94385a1
Uvantu <ptasol17@gmail.com>
2026-04-04 08:32:57 -0700

    Merge modular UI index into main

---
+++ src/ui/index.js
@@ -0,0 +1,4 @@
+export { buildRouteRoleLabel, buildRouteRoleClass } from "./helpers/route-labels.js";
+export { renderSimulationRoutes, buildRouteCard } from "./renderers/simulation-routes.js";
+export { renderAdaptiveRecommendation } from "./renderers/adaptive-recommendation.js";
+export { renderRecommendation } from "./renderers/heuristic-recommendation.js";
